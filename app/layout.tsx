5cec22ce3545fad3eaca310853aee012b7a99956
Aashir Israr <aashirisrar968@gmail.com>
2024-01-23 06:03:01 +0500

    Remove unused code and update page title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Temperature Converter App",
-  description: "Convert temperatures between Celsius and Fahrenheit.",
+  title: "Tribute Page",
+  description: "Tribute to Steve Jobs.",
 };
 
 export default function RootLayout({
@@ -16,16 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
